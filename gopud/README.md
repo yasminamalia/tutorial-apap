2ee6023971cfec5cb1c3ab7b8c00267801bf721f
yasmin.amalia <yasmin.amalia@ui.ac.id>
2019-09-18 18:10:52 +0700

    feat: add gopud feature

---
+++ gopud/README.md
@@ -0,0 +1,7 @@
+Yasmin Amalia
+1706043720
+APAP - B
+
+Pertanyaan 1: Terjadi error whitelabel error. Hal ini terjadi karena belum terdapat view atau file html yang dipanggil untuk menampilkan hasil dari program yang sudah dibuat ketika program melakukan mapping di controller.
+Pertanyaan 2: Terjadi error whitelabel error karena saat menambahkan restoran tidak ada paramater nomor telepon. Sedangkan di dalam constructor yang kita buat salah satunya adalah nomor telepon dan nomor telepon juga merupakan required parameter yang harus dimasukkan
+
---
+++ gopud/src/main/java/apap/tutorial/gopud/controller/RestoranController.java
@@ -0,0 +1,70 @@
+package apap.tutorial.gopud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import apap.tutorial.gopud.model.RestoranModel;
+import apap.tutorial.gopud.service.RestoranService;
+
+@Controller
+public class RestoranController {
+    @Autowired
+    private RestoranService restoranService;
+
+    //URL mapping add
+    @RequestMapping("/restoran/add")
+    public String add(
+            // Request Parameter untuk dipass
+            @RequestParam(value = "idRestoran", required = true) String idRestoran,
+            @RequestParam(value = "nama", required = true) String nama,
+            @RequestParam(value = "alamat", required = true) String alamat,
+            @RequestParam(value = "nomorTelepon", required = true) Integer nomorTelepon,
+            Model model
+    ){
+        // Membuat objek RestoranModel
+        RestoranModel restoran = new RestoranModel(idRestoran, nama, alamat, nomorTelepon);
+
+        // Memanggil service addRestoran
+        restoranService.addRestoran(restoran);
+
+        // Add variable nama restoran ke "namaResto" untuk dirender
+        model.addAttribute("namaResto", nama);
+
+        // Return view template
+        return "add-restoran";
+    }
+
+    // URL mapping view
+    @RequestMapping("/restoran/view")
+    public String view(
+            // Request Parameter untuk dipass
+            @RequestParam(value = "idRestoran") String idRestoran, Model model
+    ){
+        // Mengambil objek RestoranModel yang dituju
+        RestoranModel restoran = restoranService.getRestoranByIdRestoran(idRestoran);
+
+        // Add model Restoran ke "resto" untuk dirender
+        model.addAttribute("resto", restoran);
+
+        // Return View Template
+        return "view-restoran";
+    }
+
+    // URL mapping viewAll
+    @RequestMapping("/restoran/viewall")
+    public String viewAll(Model model){
+        // Mengambil semua objek RestoranModel yang ada
+        List<RestoranModel> listResto = restoranService.getRestoranList();
+
+        // Add model restoran ke "resto" untuk dirender
+        model.addAttribute("restoranList", listResto);
+
+        // Return view template
+        return "viewall-restoran";
+    }
+}
